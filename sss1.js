b5e27684de0129c8bbc246254a89fbed68f2f9cb
Asmaa Naser <165930274+Asmaa01025@users.noreply.github.com>
2024-09-24 20:55:10 +0300

    Add files via upload

---
+++ sss1.js
@@ -0,0 +1,24 @@
+document.getElementById("Asmaa").innerHTML="Asmaa Naser"
+document.getElementById("ail").innerHTML="21"
+document.getElementById("Naser").innerHTML="ALshrok"
+document.getElementById("mohammed").innerHTML="Shorouk Academy"
+document.getElementById("AhmadSS").innerHTML="Asmaa5454@gamil.com"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
